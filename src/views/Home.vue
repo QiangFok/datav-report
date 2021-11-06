c6fb44aea4c19e8cc04f4f9948fa621d856d7616
tianshow <huoqiangblack@icloud.com>
2021-11-07 03:16:54 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <top-view/>
+    <sales-view/>
+    <bottom-view/>
+    <map-view/>
+  </div>
+</template>
+
+<script>
+  import TopView from '../components/TopView'
+  import SalesView from '../components/SalesView'
+  import BottomView from '../components/BottomView'
+  import MapView from '../components/MapView'
+
+  export default {
+    name: 'Home',
+    components: {
+      TopView,
+      SalesView,
+      BottomView,
+      MapView
+    }
+  }
+</script>
+
+<style lang="scss">
+  .home {
+    width: 100%;
+    height: 100%;
+    background-color: #eeeeee;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+</style>
