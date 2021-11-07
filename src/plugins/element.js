6fd37dcaad45140045f1d02affd9057dda932240
tianshow <huoqiangblack@icloud.com>
2021-11-07 18:26:42 +0800

    + 销售趋势组件容器图表和列表

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import { Button, Card, Row, Col } from 'element-ui'
+import { Card, Row, Col, Menu, MenuItem, RadioButton, RadioGroup, DatePicker } from 'element-ui'
 
-Vue.use(Button)
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioButton)
+Vue.use(RadioGroup)
+Vue.use(DatePicker)
